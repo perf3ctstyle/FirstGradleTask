d86662d769a1c1c4e318c5e9e1d14053a7473298
nikitatorop <torop.nikita1@gmail.com>
2021-08-12 19:24:39 +0300

    Fix isPositiveNumber method call in StringUtilsTest

--- utils/src/test/java/task/StringUtilsTest.java
+++ utils/src/test/java/task/StringUtilsTest.java
@@ -7,19 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringUtilsTest {
 
-    private final StringUtils classUnderTest = new StringUtils();
-
     @Test
     public void isPositiveNumberReturnsTrueWhenGivenPositiveNumber() {
         String positiveNumber = "30";
-        boolean result = classUnderTest.isPositiveNumber(positiveNumber);
+        boolean result = StringUtils.isPositiveNumber(positiveNumber);
         assertTrue(result);
     }
 
     @Test
     public void isPositiveNumberReturnsFalseWhenGivenNegativeNumber() {
         String negativeNumber = "-20";
-        boolean result = classUnderTest.isPositiveNumber(negativeNumber);
+        boolean result = StringUtils.isPositiveNumber(negativeNumber);
         assertFalse(result);
     }
 }
