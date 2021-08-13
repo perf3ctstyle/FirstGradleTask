f4831f7c4c603bfc01c8a94be3db9c0d6ac523c5
nikitatorop <torop.nikita1@gmail.com>
2021-08-13 10:48:50 +0300

    Change isPositiveNumber implementation

--- utils/src/main/java/task/StringUtils.java
+++ utils/src/main/java/task/StringUtils.java
@@ -4,10 +4,7 @@ import static org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
 
-    private static final String MINUS = "-";
-    private static final String ZERO = "0";
-
     public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isCreatable(str) && (!str.startsWith(MINUS)) && (!str.startsWith(ZERO));
+        return NumberUtils.toDouble(str) > 0;
     }
 }
